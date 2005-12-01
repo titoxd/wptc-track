ba17db5d2ade5c346dc01c72059ff292b7368e98
jdorje <jdorje>
2005-12-01 19:46:56 +0000

    Better 2005.

--- tracks/get.sh
+++ tracks/get.sh
@@ -1,19 +1,21 @@
-storms="arlene bret cindy dennis emily franklin gert harvey irene jose katrina lee maria nate ophelia philippe rita stan tammy vince wilma alpha beta gamma delta epsilon"
-#storms="epsilon"
+#storms="arlene bret cindy dennis emily franklin gert harvey irene jose katrina lee maria nate ophelia philippe rita stan tammy vince wilma alpha beta gamma delta epsilon"
+storms="epsilon"
 
 function download() (
   for storm in $storms; do
-    wget http://www.atmos.umd.edu/~stevenb/hurr/05/$storm/$storm.txt && mv $storm.txt 2005/
+    echo "Downloading $storm."
+    (wget http://www.atmos.umd.edu/~stevenb/hurr/05/$storm/$storm.txt 2>&1 >/dev/null && mv $storm.txt 2005/) 2>&1 >/dev/null
   done
 )
 
 function generate() (
   echo "" > 2005/index.html
   for storm in $storms; do
+    echo "Generating $storm."
     ./track --input 2005/$storm.txt --format 1 && mv output.png 2005/$storm.png
     echo "<a href=$storm.png>$storm</a><br>" >> 2005/index.html
   done
 )
 
 download
-generate
+#generate
