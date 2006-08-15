af1be269609932cb1f97009306a5aa683ebdb9ab
reub2000 <reub2000>
2006-08-15 06:03:18 +0000

    format help and change hurricane to tropical storm

--- tracks/track.c
+++ tracks/track.c
@@ -56,20 +56,20 @@ static void help(void)
   printf("Create hurricane track maps\n\n");
 
   printf("Options:\n");
-  printf(" --year Select hurricanes from a specific year\n");
-  printf(" --year1, --year2 Select hurricanes from additional years\n");
-  printf(" --name Select hurricane with a specific name\n");
-  printf(" --input Use a text file to create tracking map\n");
-  printf(" --input1, --input2 Use multiple files\n");
-  printf(" --format, --format1, --format2 Set format for input files\n");
-  printf(" --res Set the horizontal resolution of output image\n");
-  printf(" --bg Set map to use for background\n");
-  printf(" --output Set the output file\n");
-  printf(" --alpha  Set transparency for storm tracks\n");
-  printf(" --dots   Set size of dots in an unknown unit\n");
-  printf(" --lines  Set size of lines in an unknown unit\n");
-  printf(" --negx   Set to non-zero value for longitude west of the prime meridian\n");
-  printf(" --negy   Set to non-zero value for latitude south of the equator\n");
+  printf("  --year			Select tropical cyclones from a specific year\n");
+  printf("  --year1, --year2 		Select tropical cyclones  from additional years\n");
+  printf("  --name 			Select tropical cyclones  with a specific name\n");
+  printf("  --input 			Use a text file to create tracking map\n");
+  printf("  --input1, --input2 		Use multiple files\n");
+  printf("  --format, --format1, --format2 Set format for input files\n");
+  printf("  --res 			Set the horizontal resolution of output image\n");
+  printf("  --bg Set 			map to use for background\n");
+  printf("  --output 			Set the output file\n");
+  printf("  --alpha  			Set transparency for storm tracks\n");
+  printf("  --dots   			Set size of dots in an unknown unit\n");
+  printf("  --lines  			Set size of lines in an unknown unit\n");
+  printf("  --negx   			Set to non-zero value for longitude west of the prime meridian\n");
+  printf("  --negy   			Set to non-zero value for latitude south of the equator\n");
 }
 
 static struct args read_args(int argc, char **argv)
