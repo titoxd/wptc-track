1cc50c2730346ec7f1850dd3ecdac9358def009b
titoxd <titoxd.wikimedia@gmail.com>
2011-06-21 00:29:04 +0000

    Cleanup ATCF handler now that we are not ignoring invests anymore

--- tracks/atcf.c
+++ tracks/atcf.c
@@ -38,7 +38,6 @@ struct stormdata *read_stormdata_atcf(struct stormdata *storms,
 	struct storm storm;
 	char *token[35], *datestamp;
 	char ds[5];
-	int trackstarted = 0;
 	int dateset = 0;
 	int lasttype;
 	int year; 
@@ -67,109 +66,102 @@ struct stormdata *read_stormdata_atcf(struct stormdata *storms,
 			i++;
 		}
 		
-		/* /\* Check that we are a tropical depression, or storm, or anything but a darn INVEST *\/ */
-		/* if (strcasecmp(token[10], "DB") != 0) { */
-			trackstarted = 1;  /* true */
-		/* } */
+		/* Begin storing data */
+		/* Check the storm's characteristics for this time point */
+		if ((strcasecmp(token[10], "TD") == 0) || (strcasecmp(token[10], "TS") == 0) || (strcasecmp(token[10], "TY") == 0) || 
+			(strcasecmp(token[10], "ST") == 0) || (strcasecmp(token[10], "TC") == 0) || (strcasecmp(token[10], "HU") == 0)) {
+			pos.type = TROPICAL;
+			lasttype = TROPICAL;
+		} else if ((strcasecmp(token[10], "SD") == 0) || (strcasecmp(token[10], "SS") == 0)) {
+			pos.type = SUBTROPICAL;
+			lasttype = SUBTROPICAL;
+		} else if ((strcasecmp(token[10], "EX") == 0)) {
+			pos.type = EXTRATROPICAL;
+			lasttype = EXTRATROPICAL;
+		} else if ((strcasecmp(token[10], "LO") == 0) || (strcasecmp(token[10], "WV") == 0) || (strcasecmp(token[10], "DB") == 0)) {
+			pos.type = LOW;
+			lasttype = LOW;
+		} else {
+			/* If we have a different condition (e.g. inland, dissipating), 
+			 * assume that the last valid condition is still true */
+			pos.type = lasttype;
+		}
 		
-		if (trackstarted) {
-			
-			/* Begin storing data */
-			/* Check the storm's characteristics for this time point */
-			if ((strcasecmp(token[10], "TD") == 0) || (strcasecmp(token[10], "TS") == 0) || (strcasecmp(token[10], "TY") == 0) || 
-				(strcasecmp(token[10], "ST") == 0) || (strcasecmp(token[10], "TC") == 0) || (strcasecmp(token[10], "HU") == 0)) {
-				pos.type = TROPICAL;
-				lasttype = TROPICAL;
-			} else if ((strcasecmp(token[10], "SD") == 0) || (strcasecmp(token[10], "SS") == 0)) {
-				pos.type = SUBTROPICAL;
-				lasttype = SUBTROPICAL;
-			} else if ((strcasecmp(token[10], "EX") == 0)) {
-				pos.type = EXTRATROPICAL;
-				lasttype = EXTRATROPICAL;
-			} else if ((strcasecmp(token[10], "LO") == 0) || (strcasecmp(token[10], "WV") == 0) || (strcasecmp(token[10], "DB") == 0)) {
-				pos.type = LOW;
-				lasttype = LOW;
-			} else {
-				/* If we have a different condition (e.g. inland, dissipating), 
-				 * assume that the last valid condition is still true */
-				pos.type = lasttype;
+		/* Set the storm name and ID */
+		if ((strcasecmp(token[10], "TS") == 0) || (strcasecmp(token[10], "TY") == 0) || (strcasecmp(token[10], "ST") == 0) || 
+			(strcasecmp(token[10], "TC") == 0) || (strcasecmp(token[10], "HU") == 0) || (strcasecmp(token[10], "SS") == 0)) {
+			if (strcasecmp(token[27], storm.header.name) != 0) {
+				strncpy(storm.header.name, token[27], sizeof(token[27]));
+				storm.header.id = atoi(token[1]);
 			}
-
-			/* Set the storm name and ID */
-			if ((strcasecmp(token[10], "TS") == 0) || (strcasecmp(token[10], "TY") == 0) || (strcasecmp(token[10], "ST") == 0) || 
-				(strcasecmp(token[10], "TC") == 0) || (strcasecmp(token[10], "HU") == 0) || (strcasecmp(token[10], "SS") == 0)) {
-				if (strcasecmp(token[27], storm.header.name) != 0) {
-					strncpy(storm.header.name, token[27], sizeof(token[27]));
-					storm.header.id = atoi(token[1]);
-				}
-			}
-
-			if (!dateset) {
-				datestamp = strdup(token[2]);
-				strncpy(ds, datestamp+0,4); 
-				ds[4] = '\0';
-				storm.header.year = atoi(ds);
-
-				datestamp = strdup(token[2]);
-				strncpy(ds, datestamp+4,2);  
-				ds[2] = '\0';
-				storm.header.month = atoi(ds);
-
-				datestamp = strdup(token[2]);
-				strncpy(ds, datestamp+6,2);
-				ds[2] = '\0';
-				storm.header.day = atoi(ds);
-				dateset = 1;
-			}
-			
-			/* month, day, hour */
+		}
+		
+		if (!dateset) {
 			datestamp = strdup(token[2]);
-			strncpy(ds, datestamp+0,4);
+			strncpy(ds, datestamp+0,4); 
 			ds[4] = '\0';
-			year = atoi(ds);
-
+			storm.header.year = atoi(ds);
+			
 			datestamp = strdup(token[2]);
-			strncpy(ds, datestamp+4,2);
+			strncpy(ds, datestamp+4,2);  
 			ds[2] = '\0';
-			pos.month = atoi(ds);
-
+			storm.header.month = atoi(ds);
+			
 			datestamp = strdup(token[2]);
 			strncpy(ds, datestamp+6,2);
 			ds[2] = '\0';
-			pos.day = atoi(ds);
-
-			datestamp = strdup(token[2]);
-			strncpy(ds, datestamp+8,2);
-			ds[2] = '\0';
-			pos.hour = atoi(ds);
-			
-			/* latitude */
-			if (token[6][strlen(token[6])-1] == 'N') {
-				pos.lat = (double)atoi(token[6]) / 10.0;
-			} else {
-				/* TODO: check this actually works */
-				pos.lat = -(double)atoi(token[6]) / 10.0;
-			}
-
-			if (token[7][strlen(token[7])-1] == 'W') {
-				pos.lon = (double)atoi(token[7]) / 10.0;
-			} else {
-				/* TODO: check this actually works */
-				pos.lon = -(double)atoi(token[7]) / 10.0;
-			}
-			
-			/* wind */
-			pos.wind = atoi(token[8]);
-			
-			/* pressure */ 
-			pos.pres = atoi(token[9]);
-			
-			if (pos.wind == 0) {
-				break;
-			}
-			
-			save_pos(args, storms, &storm, &pos);
+			storm.header.day = atoi(ds);
+			dateset = 1;
 		}
+		
+		/* month, day, hour */
+		datestamp = strdup(token[2]);
+		strncpy(ds, datestamp+0,4);
+		ds[4] = '\0';
+		year = atoi(ds);
+		
+		datestamp = strdup(token[2]);
+		strncpy(ds, datestamp+4,2);
+		ds[2] = '\0';
+		pos.month = atoi(ds);
+		
+		datestamp = strdup(token[2]);
+		strncpy(ds, datestamp+6,2);
+		ds[2] = '\0';
+		pos.day = atoi(ds);
+		
+		datestamp = strdup(token[2]);
+		strncpy(ds, datestamp+8,2);
+		ds[2] = '\0';
+		pos.hour = atoi(ds);
+		
+		/* latitude */
+		if (token[6][strlen(token[6])-1] == 'N') {
+			pos.lat = (double)atoi(token[6]) / 10.0;
+		} else {
+			/* TODO: check this actually works */
+			pos.lat = -(double)atoi(token[6]) / 10.0;
+		}
+		
+		if (token[7][strlen(token[7])-1] == 'W') {
+			pos.lon = (double)atoi(token[7]) / 10.0;
+		} else {
+			/* TODO: check this actually works */
+			pos.lon = -(double)atoi(token[7]) / 10.0;
+		}
+		
+		/* wind */
+		pos.wind = atoi(token[8]);
+		
+		/* pressure */ 
+		pos.pres = atoi(token[9]);
+		
+		if (pos.wind == 0) {
+			break;
+		}
+		
+		save_pos(args, storms, &storm, &pos);
+		
 	}
 	fclose(file);
 	
