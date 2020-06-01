titoxd/wptc-track

commit 9788672f9b0ca4c756e3cc19e9c51695e60ead64

Handle subtrop depression

tracks/tcr.c
@@ -96,7 +96,8 @@ struct stormdata *read_stormdata_tcr(struct stormdata *storms,
 	       || strcasecmp(line, "remnant low") == 0
 	       || strcasecmp(line, "tropical wave") == 0) {
       type = LOW;
-    } else if (strcasecmp(line, "subtropical storm") == 0) {
+    } else if (strcasecmp(line, "subtropical depression") == 0) 
+               || strcasecmp(line, "subtropical storm") == 0) {
       type = SUBTROPICAL;
     } else if (strcmp(line, "\"") == 0
 	       || strcmp(line, "\223") == 0) {

commit 5c7ce80832ac27765309ff41ddb3abb01022a8ab

Fix syntax error introduced by 9788672f9b0ca4c756e3cc19e9c51695e60ead64

tracks/tcr.c
@@ -96,7 +96,7 @@ struct stormdata *read_stormdata_tcr(struct stormdata *storms,
 	       || strcasecmp(line, "remnant low") == 0
 	       || strcasecmp(line, "tropical wave") == 0) {
       type = LOW;
-    } else if (strcasecmp(line, "subtropical depression") == 0) 
+    } else if ((strcasecmp(line, "subtropical depression") == 0) 
                || strcasecmp(line, "subtropical storm") == 0) {
       type = SUBTROPICAL;
     } else if (strcmp(line, "\"") == 0